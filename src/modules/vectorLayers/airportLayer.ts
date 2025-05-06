d3395d3fdd5b53bc89939c7fe84f178559bfba95
amaliye <amaliesanchez@outlook.com>
2025-05-06 15:33:52 +0200

    Added icons to airport and trainstationlayers

--- src/modules/vectorLayers/airportLayer.ts
+++ src/modules/vectorLayers/airportLayer.ts
@@ -1,10 +1,20 @@
 import VectorLayer from "ol/layer/Vector";
 import VectorSource from "ol/source/Vector";
 import { GeoJSON } from "ol/format";
+import { Icon, Style, Circle as CircleStyle, Fill, Stroke } from "ol/style";
+
+const airplaneIconStyle = new Style({
+  image: new Icon({
+    src: "/icons/fluent-emoji--airplane.svg",
+    scale: 1.2,
+    anchor: [0.5, 1],
+  }),
+});
 
 export const airportLayer = new VectorLayer({
   source: new VectorSource({
     url: "geojson/flyplasser.geojson",
     format: new GeoJSON(),
   }),
+  style: airplaneIconStyle,
 });
--- src/modules/vectorLayers/trainStationLayer.ts
+++ src/modules/vectorLayers/trainStationLayer.ts
@@ -1,10 +1,20 @@
 import VectorLayer from "ol/layer/Vector";
 import VectorSource from "ol/source/Vector";
 import { GeoJSON } from "ol/format";
+import { Icon, Style } from "ol/style";
+
+const trainIconStyle = new Style({
+  image: new Icon({
+    src: "/icons/fluent-emoji--station.svg",
+    scale: 1,
+    anchor: [0.5, 1],
+  }),
+});
 
 export const trainStationLayer = new VectorLayer({
   source: new VectorSource({
     url: "geojson/jernbanestasjoner.geojson",
     format: new GeoJSON(),
   }),
+  style: trainIconStyle,
 });
