28ef9d5d11f3c0573eb746cd36f672ac964a6975
SigurdPJ <sijo063@student.kristiania.no>
2025-05-08 00:57:43 +0200

    changed server.ts (heroku fix?)

--- server/server.ts
+++ server/server.ts
@@ -1,40 +1,31 @@
 import { Hono } from "hono";
 import { serve } from "@hono/node-server";
 import pg from "pg";
+import { serveStatic } from "@hono/node-server/serve-static";
 
 const connectionString = process.env.DATABASE_URL;
 const postgresql = connectionString
   ? new pg.Pool({ connectionString, ssl: { rejectUnauthorized: false } })
   : new pg.Pool({ user: "postgres", password: "postgres" });
 
-const schema = "kulturminner_178e81c5b4f1432e84e3e50b55042a3e";
-
 const app = new Hono();
-
-app.get("/", (c) => c.text("Welcome to the Hono API!"));
-
 app.get("/api/culturalheritage", async (c) => {
-  const result = await postgresql.query(`
-      SELECT navn, opphav, informasjon,
-             ST_AsGeoJSON(ST_Transform(lokalitet.omrade, 4326)) AS geometry
-      FROM ${schema}.lokalitet
-      WHERE synlig = true
-      LIMIT 1000
-  `);
-
+  const result = await postgresql.query(
+    "SELECT navn, opphav, st_transform(st_simplify(omrade, 100), 4326)::json as geometry FROM kulturminner_178e81c5b4f1432e84e3e50b55042a3e.lokalitet WHERE synlig = true LIMIT 1000",
+  );
   return c.json({
     type: "FeatureCollection",
-    crs: {
-      type: "name",
-    },
-    features: result.rows.map(({ geometry, ...props }) => ({
-      type: "Feature",
-      properties: props,
-      geometry: JSON.parse(geometry),
-    })),
+    crs: { type: "name", properties: { name: "ESPG:4326" } },
+    features: result.rows
+      .filter((row) => row.geometry !== null) // filter out null geometry
+      .map(({ geometry: { coordinates, type }, ...properties }) => ({
+        type: "Feature",
+        geometry: { type, coordinates },
+        properties,
+      })),
   });
 });
+app.use("*", serveStatic({ root: "../dist" }));
 
 const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
-console.log(`Server running on port ${port}`);
 serve({ fetch: app.fetch, port });
