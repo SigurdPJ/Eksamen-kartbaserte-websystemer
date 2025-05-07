e02aa08783ad78aa83baa2bfc88a4f16677d4251
amaliye <amaliesanchez@outlook.com>
2025-05-07 19:49:13 +0200

    Fixed airportoverlay, doesnt show altnavn if the airport doesnt have one

--- src/modules/components/AirportOverlay.tsx
+++ src/modules/components/AirportOverlay.tsx
@@ -1,18 +1,21 @@
 import React from "react";
-
 import AirportProps from "../interfaces/AirportProps";
 
 const AirportOverlay = ({ features }: { features: AirportProps[] }) => {
   if (features.length >= 1) {
+    const airport = features[0];
+
     return (
       <div className={"overlayDiv"}>
         <h3>Flyplassinfo</h3>
         <p>
-          <strong>Flyplassnavn:</strong> {features[0].navn}
+          <strong>Flyplassnavn:</strong> {airport.navn}
         </p>
-        <p>
-          <strong>Alternativt navn:</strong> {features[0].alt_navn}
-        </p>
+        {airport.alt_navn && (
+          <p>
+            <strong>Alternativt navn:</strong> {airport.alt_navn}
+          </p>
+        )}
       </div>
     );
   }
