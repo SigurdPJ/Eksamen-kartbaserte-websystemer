86bed79835bf74f9ba631d8ce3744b7c4fc4019b
SigurdPJ <sijo063@student.kristiania.no>
2025-05-08 18:15:20 +0200

    added clusters to airportLayer.ts

--- src/modules/vectorLayers/airportLayer.ts
+++ src/modules/vectorLayers/airportLayer.ts
@@ -1,20 +1,66 @@
 import VectorLayer from "ol/layer/Vector";
 import VectorSource from "ol/source/Vector";
+import Cluster from "ol/source/Cluster";
 import { GeoJSON } from "ol/format";
-import { Icon, Style, Circle as CircleStyle, Fill, Stroke } from "ol/style";
+import { Style, Fill, Text, Icon } from "ol/style";
+import { FeatureLike } from "ol/Feature";
+import Point from "ol/geom/Point";
+import Feature from "ol/Feature";
+import Geometry from "ol/geom/Geometry";
+import MultiPoint from "ol/geom/MultiPoint";
 
-const airplaneIconStyle = new Style({
-  image: new Icon({
-    src: "/icons/fluent-emoji--airplane.png",
-    scale: 0.5,
-    anchor: [0.5, 1],
-  }),
+const geometryFunction = (feature: Feature<Geometry>): Point | null => {
+  const geometry = feature.getGeometry();
+
+  if (!geometry) return null;
+
+  switch (geometry.getType()) {
+    case "Point":
+      return geometry as Point;
+    case "MultiPoint":
+      const multiPoint = geometry as MultiPoint;
+      const coordinates = multiPoint.getCoordinates();
+      return coordinates.length > 0 ? new Point(coordinates[0]) : null;
+    default:
+      return null;
+  }
+};
+
+const airportSource = new VectorSource({
+  url: "geojson/flyplasser.geojson",
+  format: new GeoJSON(),
 });
 
+const clusterSource = new Cluster({
+  distance: 40,
+  minDistance: 20,
+  source: airportSource,
+  geometryFunction: geometryFunction,
+});
+
+const styleCache: Record<number, Style> = {};
+
+const clusterStyle = (feature: FeatureLike) => {
+  const size = feature.get("features").length;
+  if (!styleCache[size]) {
+    styleCache[size] = new Style({
+      image: new Icon({
+        src: "/icons/fluent-emoji--airplane.png",
+        scale: 0.5,
+        anchor: [0.5, 1],
+      }),
+      text: new Text({
+        text: size > 1 ? size.toString() : "",
+        fill: new Fill({ color: "black" }),
+        font: "bold 16px Arial",
+        offsetY: 8,
+      }),
+    });
+  }
+  return styleCache[size];
+};
+
 export const airportLayer = new VectorLayer({
-  source: new VectorSource({
-    url: "geojson/flyplasser.geojson",
-    format: new GeoJSON(),
-  }),
-  style: airplaneIconStyle,
+  source: clusterSource,
+  style: clusterStyle,
 });
